638a609ac5bae250ca0489f8f253261d74d6e05f
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-01-31 15:17:36 +0900

    first commit

---
+++ FloatingPanelSample/ContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContentViewController.swift
+//  FloatingPanelSample
+//
+//  Created by 白数叡司 on 2021/01/31.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
